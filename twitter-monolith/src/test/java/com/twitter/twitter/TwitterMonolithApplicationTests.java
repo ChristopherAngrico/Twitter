5bd9951bac12ae6548caedbfc098fdf08a081576
Christopher Angrico <christopherangrico82@gmail.com>
2021-08-01 13:03:20 +0700

    initial project

---
+++ twitter-monolith/src/test/java/com/twitter/twitter/TwitterMonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.twitter.twitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwitterMonolithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
